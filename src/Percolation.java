79dcc7633961901a71510611c6565dcb969a078f
teleport1995 <sobolandrey1995@gmail.com>
2015-01-27 12:24:23 +0200

    custom UF and custom BitSet -> little memory

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,28 +1,70 @@
-import java.util.BitSet;
-
 public class Percolation {
-    private BitSet a;
-    private BitSet full;
+
+    private class MyBitSet {
+        private byte[] a;
+
+        public MyBitSet(int N) {
+            a = new byte[(N - 1) / 8 + 1];
+        }
+
+        public boolean get(int x) {
+            return (a[x / 8] & (1 << (x % 8))) != 0;
+        }
+
+        public void set(int x) {
+            a[x / 8] |= 1 << (x % 8);
+        }
+
+    }
+
+    private class MyUF {
+        private int[] id;
+
+        public MyUF(int N) {
+            id = new int[N];
+            for (int i = 0; i < N; i++) {
+                id[i] = i;
+            }
+        }
+        public int find(int p) {
+            if (id[p] == p)
+                return p;
+            id[p] = find(id[p]);
+            return id[p];
+        }
+        public boolean connected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            id[rootQ] = id[rootP];
+        }
+    }
+
+    private MyBitSet a;
+    private MyBitSet full;
     private int n;
-    private WeightedQuickUnionUF uf;
-    private int[] c = {0, 0, -1, 1};
-    private int[] d = {-1, 1, 0, 0};
+    private MyUF uf;
+    private WeightedQuickUnionUF dop;
 
     public Percolation(int N) {
         if (N <= 0)
             throw new IllegalArgumentException("exception");
         n = N;
-        a = new BitSet(N * N);
-        full = new BitSet(N * N);
-        uf = new WeightedQuickUnionUF(n * n + 2);
+        a = new MyBitSet(N * N);
+        full = new MyBitSet(N * N);
+        uf = new MyUF(n * n + 2);
+        dop = new WeightedQuickUnionUF(2);
     }
 
-    private void dfs(int i, int j) {
+    private void dfs(int i, int j, int[] c, int [] d) {
         full.set(xyto1D(i, j));
         for (int k = 0; k < 4; k++) {
             if (isValid(i + c[k], j + d[k]) && a.get(xyto1D(i+c[k], j+d[k]))
                     && !full.get(xyto1D(i+c[k], j+d[k]))) {
-                dfs(i + c[k], j + d[k]);
+                dfs(i + c[k], j + d[k], c, d);
             }
         }
     }
@@ -32,6 +74,9 @@ public class Percolation {
             throw new IndexOutOfBoundsException("exception");
         if (isOpen(i, j))
             return;
+        dop.union(0, 1);
+        int[] c = {0, 0, -1, 1};
+        int[] d = {-1, 1, 0, 0};
         a.set(xyto1D(i, j));
         boolean needtofull = false;
         if (i == 1)
@@ -43,7 +88,7 @@ public class Percolation {
                 uf.union(xyto1D(i, j), xyto1D(i + c[k], j + d[k]));
             }
         if (needtofull)
-            dfs(i, j);
+            dfs(i, j, c, d);
         if (i == 1)
             uf.union(xyto1D(i, j), n * n);
         if (i == n)
@@ -71,6 +116,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
+        dop.connected(0, 1);
         return uf.connected(n * n, n * n + 1);
     }
 
